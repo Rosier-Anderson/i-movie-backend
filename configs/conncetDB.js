f236d83230276c985d2a8a02a0a78cf913bcb8a6
CodeSandbox <support@codesandbox.io>
2025-09-19 09:23:56 +0000

    Sandbox snapshot commit

--- configs/conncetDB.js
+++ configs/conncetDB.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // DB uri here
+    await mongoose.connect(process.env.URI);
   } catch (err) {
     console.log(err);
   }
