45fb701a6a2d416202924bcb08f4dce2c02ace1c
CodeSandbox <support@codesandbox.io>
2025-09-21 06:28:46 +0000

    Sandbox snapshot commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,21 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const UserModel = require("../model/User");
 
-const handleAuthUser = async (req, res) => {
+const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
 
   try {
-    return res.status(400).json({ Erorr: `${user} and ${pwd} is required.` });
+    if (!user || !pwd)
+      return res
+        .status(400)
+        .json({ Erorr: `${user} and ${pwd} are required.` });
+    const foundUser = UserModel.findOne({ username: user });
+    if (!foundUser) return res.sendStatus(401);
+    const matchUser = await bcrypt.compare(pwd, foundUser.pwd);
+    if (!matchUser) return res.sendStatus(401);
   } catch (err) {
     res.status(500).json({ msg: err.mesage });
   }
 };
+
+module.exports = { handleLogin };
