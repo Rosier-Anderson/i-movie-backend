7086ec490280cce5ed75c9fa066e911b815a5cf8
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-10-08 13:10:58 -0400

    Adding locals dev cors

--- configs/corsOptions.js
+++ configs/corsOptions.js
@@ -1,4 +1,3 @@
-
 const whitelist = [
   "https://56t6mg-3000.csb.app",
   "https://vercel.com",
@@ -6,15 +5,18 @@ const whitelist = [
   "https://dashboard.render.com",
   "https://i-movie-backend.onrender.com/",
   "http://localhost:3000/login",
-  "162.120.186.89"
-
+  "162.120.186.89",
+  "https://www.google.com/",
+  "http://localhost:3000",                // local dev
+  "https://my-frontend.vercel.app",      // your deployed frontend
+  "https://i-movie-backend.onrender.com" // optional, backend domain
 ];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS") );
+      callback(new Error("Not allowed by CORS"));
     }
   },
 };
