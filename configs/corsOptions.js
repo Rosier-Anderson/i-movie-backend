52dcafcbdd623b9e5744dea46155e74080c067e1
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-10-08 17:19:22 -0400

    Fixing cors origin and  headers

--- configs/corsOptions.js
+++ configs/corsOptions.js
@@ -1,15 +1,19 @@
 const whitelist = [
-  "http://localhost:3000",                 // local dev
-  "https://my-frontend.vercel.app",       // your deployed frontend
-  "https://i-movie-backend.onrender.com", // backend domain itself
-  "undefined"
+  "http://localhost:3500", // local dev
+  "https://my-frontend.vercel.app", // your deployed frontend
+  "https://i-movie-backend.onrender.com", // optional, backend domai
 ];
-
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    console.log(origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
-    } 
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
   },
+    optionsSucessStatus: 200,
+  credentials: true,
+  methods: "GET,POST,PUT,DELETE",
 };
 module.exports = corsOptions;
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,14 @@
+const { logger } = require("../middleware/logEvents");
 const UserModel = require("../model/User");
 
 const getUser = async  (req, res) => {
+    try{
+
+    }catch(e){
+        logger(req, res)
+    }
 const user = await UserModel.find().exec()
+
 return res.status(200).json({user: 
     user
 })
