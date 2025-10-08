550eb17de6d760b25c786554ab300732b6ff19ee
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-10-08 13:23:06 -0400

    Cors

--- configs/corsOptions.js
+++ configs/corsOptions.js
@@ -1,16 +1,9 @@
 const whitelist = [
-  "https://56t6mg-3000.csb.app",
-  "https://vercel.com",
-  "https://render.com",
-  "https://dashboard.render.com",
-  "https://i-movie-backend.onrender.com/",
-  "http://localhost:3000/login",
-  "162.120.186.89",
-  "https://www.google.com/",
-  "http://localhost:3000",                // local dev
-  "https://my-frontend.vercel.app",      // your deployed frontend
-  "https://i-movie-backend.onrender.com" // optional, backend domain
+  "http://localhost:3000",                 // local dev
+  "https://my-frontend.vercel.app",       // your deployed frontend
+  "https://i-movie-backend.onrender.com"  // backend domain itself
 ];
+
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
