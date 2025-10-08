38ef370902f003132230773e97011164f6d4a4ef
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-10-08 13:31:52 -0400

    Alllowed all origins temporarily

--- configs/corsOptions.js
+++ configs/corsOptions.js
@@ -1,16 +1,15 @@
 const whitelist = [
   "http://localhost:3000",                 // local dev
   "https://my-frontend.vercel.app",       // your deployed frontend
-  "https://i-movie-backend.onrender.com"  // backend domain itself
+  "https://i-movie-backend.onrender.com", // backend domain itself
+  "undefined"
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    } 
   },
 };
 module.exports = corsOptions;
